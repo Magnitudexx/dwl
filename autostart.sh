df0f9fdb094df030d1d029a3f61c4d1d6fe565aa
Philip Lundström <init.defaultBranch>
2021-12-09 13:42:59 +0100

    added tag viewer , see waybar

--- autostart.sh
+++ autostart.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
-
 swaybg -i ~/wallpaper/wp5102697-neon-retro-city-ps4-wallpapers.jpg &
-waybar &
 swayidle -w \
 timeout 300 'swaylock -f -i $wallpaper' \
 timeout 305 'swaymsg "output * dpms off"' \
 resume 'swaymsg "output * dpms on"'\
 before-sleep 'swaylock -f -i $wallpaper' &
 mako &
+tee | ./dwlwrapper.sh &
+waybar &
+
+
 
